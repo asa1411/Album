cc9bdf6c42667df29bde4d930ea0055347340af9
asa1411 <45476911+asa1411@users.noreply.github.com>
2021-07-07 22:20:16 -0500

    changes

--- ask/src/cell/index.js
+++ ask/src/cell/index.js
@@ -1,7 +1,11 @@
 let countEl = document.getElementById("count-el");
+
 let count = 0;
 function increment (){
     count += 1;
     countEl.innerText = count;
 }
+function save (){
+    countEl.innerText = count;
 
+}
